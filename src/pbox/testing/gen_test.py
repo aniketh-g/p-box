b300451f7b5d188e82e08638ad775a965fa1d3d0
Aniketh Ganesh <aniketh.ganesh@gmail.com>
2024-01-09 23:17:17 +0530

    Implemented 8-bit multiplication tests

--- src/pbox/testing/gen_test.py
+++ src/pbox/testing/gen_test.py
@@ -1,6 +1,10 @@
 import numpy as np
 import psimd_test_fns
-from psimd_test_fns import smul8
+from psimd_test_fns import *
+import random
+from random import randrange
+
+N = 5
 
 computeTB = open("../computeTB.bsv", "w")
 computeTB.write("`include \"compute.bsv\"\n\
@@ -12,18 +16,19 @@ module mkTest(Empty);\n\
 \n\
     rule count;\n\
         counter <= counter + 1;\n\
-        if(counter >= 50) begin\n\
+        if(counter >= {N}) begin\n\
             $display(\"[TB] Timeout\");\n\
             $finish;\n\
         end\n\
     endrule\n\
 \
-    rule compute;")
+    rule compute;".format(N=N+10))
 computeTB.close()
 
 pyout = open("./outputs/pyout.txt", "w")
-i=1
-pyout.write("[TB {j}] {ans}\n".format(ans=smul8(np.int64(35134353456),np.int64(435346546), i), j=i))
+
+for i in range(N):
+    pyout.write("[TB {j}] {ans}\n".format(ans=smul8(np.int64(randrange(-2**63,2**63-1)),np.int64(randrange(-2**63,2**63-1)), i), j=i))
 
 
 pyout.close()
@@ -32,4 +37,4 @@ pyout.write("[TB] Timeout")
 computeTB = open("../computeTB.bsv", "a")
 computeTB.write("\n\tendrule\nendmodule")
 computeTB.close()
-print("finished")
+print("Generated {N} test cases".format(N=N))
--- src/pbox/testing/psimd_test_fns.py
+++ src/pbox/testing/psimd_test_fns.py
@@ -5,7 +5,7 @@ warnings.filterwarnings("ignore")
 debug = False
 sep = ''
 
-def sm8(a, b, isSigned, isSaturate):
+def mul8(a, b, isSigned, isSaturate):
     ov = 0
     # Ensure the input values are 8-bit
     a = a & 0xFF
@@ -31,7 +31,7 @@ def sm8(a, b, isSigned, isSaturate):
     # Handle overflow by taking the least significant 8 bits
     return [format(result & 0xFFFF, "016b"), ov]
 
-def smul8(a, b, i): #1010100AAAAABBBBB000DDDDD1110111
+def smul8(a, b, i):
     funct7 = "1010100"
     funct3 = "000"
     f = open("../computeTB.bsv", "a")
@@ -39,12 +39,110 @@ def smul8(a, b, i): #1010100AAAAABBBBB000DDDDD1110111
             .format(f7=funct7, f3=funct3, rs1=a, rs2=b, rd=0, rs1addr="00000", rs2addr="00000", rdaddr="00000", opcode="1110111", o='{', c='}', j=i))
     f.close()
 
-    a8s = list((a >> i) & 0xFF for i in range(0,64,8))
-    b8s = list((b >> i) & 0xFF for i in range(0,64,8))
+    a8s = list((a >> j) & 0xFF for j in range(0,64,8))
+    b8s = list((b >> j) & 0xFF for j in range(0,64,8))
     if(debug): print(a8s, b8s)
-    res = sm8(a8s[3], b8s[3], True, False)[0] + sep +\
-          sm8(a8s[2], b8s[2], True, False)[0] + sep +\
-          sm8(a8s[1], b8s[1], True, False)[0] + sep +\
-          sm8(a8s[0], b8s[0], True, False)[0]
+    res = mul8(a8s[3], b8s[3], True, False)[0] + sep +\
+          mul8(a8s[2], b8s[2], True, False)[0] + sep +\
+          mul8(a8s[1], b8s[1], True, False)[0] + sep +\
+          mul8(a8s[0], b8s[0], True, False)[0]
+    if(debug): print(res)
+    return res
+
+def smulx8(a, b, i):
+    funct7 = "1010101"
+    funct3 = "000"
+    f = open("../computeTB.bsv", "a")
+    f.write("""\n\t\tif(counter == {j}) $display("[TB {j}] %b", fn_compute(PBoxIn{o}instr: 32'b{f7}{rs1addr}{rs2addr}{f3}{rdaddr}{opcode}, rs1: 64'd{rs1}, rs2: 64'd{rs2}, rd:  64'd{rd}{c}).data);"""\
+            .format(f7=funct7, f3=funct3, rs1=a, rs2=b, rd=0, rs1addr="00000", rs2addr="00000", rdaddr="00000", opcode="1110111", o='{', c='}', j=i))
+    f.close()
+
+    a8s = list((a >> j) & 0xFF for j in range(0,64,8))
+    b8s = list((b >> j) & 0xFF for j in range(0,64,8))
+    if(debug): print(a8s, b8s)
+    res = mul8(a8s[3], b8s[2], True, False)[0] + sep +\
+          mul8(a8s[2], b8s[3], True, False)[0] + sep +\
+          mul8(a8s[1], b8s[0], True, False)[0] + sep +\
+          mul8(a8s[0], b8s[1], True, False)[0]
+    if(debug): print(res)
+    return res
+
+def umul8(a, b, i):
+    funct7 = "1011100"
+    funct3 = "000"
+    f = open("../computeTB.bsv", "a")
+    f.write("""\n\t\tif(counter == {j}) $display("[TB {j}] %b", fn_compute(PBoxIn{o}instr: 32'b{f7}{rs1addr}{rs2addr}{f3}{rdaddr}{opcode}, rs1: 64'd{rs1}, rs2: 64'd{rs2}, rd:  64'd{rd}{c}).data);"""\
+            .format(f7=funct7, f3=funct3, rs1=a, rs2=b, rd=0, rs1addr="00000", rs2addr="00000", rdaddr="00000", opcode="1110111", o='{', c='}', j=i))
+    f.close()
+
+    a8s = list((a >> j) & 0xFF for j in range(0,64,8))
+    b8s = list((b >> j) & 0xFF for j in range(0,64,8))
+    if(debug): print(a8s, b8s)
+    res = mul8(a8s[3], b8s[3], False, False)[0] + sep +\
+          mul8(a8s[2], b8s[2], False, False)[0] + sep +\
+          mul8(a8s[1], b8s[1], False, False)[0] + sep +\
+          mul8(a8s[0], b8s[0], False, False)[0]
+    if(debug): print(res)
+    return res
+
+def umulx8(a, b, i):
+    funct7 = "1011101"
+    funct3 = "000"
+    f = open("../computeTB.bsv", "a")
+    f.write("""\n\t\tif(counter == {j}) $display("[TB {j}] %b", fn_compute(PBoxIn{o}instr: 32'b{f7}{rs1addr}{rs2addr}{f3}{rdaddr}{opcode}, rs1: 64'd{rs1}, rs2: 64'd{rs2}, rd:  64'd{rd}{c}).data);"""\
+            .format(f7=funct7, f3=funct3, rs1=a, rs2=b, rd=0, rs1addr="00000", rs2addr="00000", rdaddr="00000", opcode="1110111", o='{', c='}', j=i))
+    f.close()
+
+    a8s = list((a >> j) & 0xFF for j in range(0,64,8))
+    b8s = list((b >> j) & 0xFF for j in range(0,64,8))
+    if(debug): print(a8s, b8s)
+    res = mul8(a8s[3], b8s[2], False, False)[0] + sep +\
+          mul8(a8s[2], b8s[3], False, False)[0] + sep +\
+          mul8(a8s[1], b8s[0], False, False)[0] + sep +\
+          mul8(a8s[0], b8s[1], False, False)[0]
+    if(debug): print(res)
+    return res
+
+def khm8(a, b, i):
+    funct7 = "1000111"
+    funct3 = "000"
+    f = open("../computeTB.bsv", "a")
+    f.write("""\n\t\tif(counter == {j}) $display("[TB {j}] %b", fn_compute(PBoxIn{o}instr: 32'b{f7}{rs1addr}{rs2addr}{f3}{rdaddr}{opcode}, rs1: 64'd{rs1}, rs2: 64'd{rs2}, rd:  64'd{rd}{c}).data);"""\
+            .format(f7=funct7, f3=funct3, rs1=a, rs2=b, rd=0, rs1addr="00000", rs2addr="00000", rdaddr="00000", opcode="1110111", o='{', c='}', j=i))
+    f.close()
+
+    a8s = list((a >> j) & 0xFF for j in range(0,64,8))
+    b8s = list((b >> j) & 0xFF for j in range(0,64,8))
+    if(debug): print(a8s, b8s)
+    res = (mul8(a8s[7], b8s[7], True, True)[0]>>7 & 0xF) + sep +\
+          (mul8(a8s[6], b8s[6], True, True)[0]>>7 & 0xF) + sep +\
+          (mul8(a8s[5], b8s[5], True, True)[0]>>7 & 0xF) + sep +\
+          (mul8(a8s[4], b8s[4], True, True)[0]>>7 & 0xF) + sep +\
+          (mul8(a8s[3], b8s[3], True, True)[0]>>7 & 0xF) + sep +\
+          (mul8(a8s[2], b8s[2], True, True)[0]>>7 & 0xF) + sep +\
+          (mul8(a8s[1], b8s[1], True, True)[0]>>7 & 0xF) + sep +\
+          (mul8(a8s[0], b8s[0], True, True)[0]>>7 & 0xF)
+    if(debug): print(res)
+    return res
+
+def khm8(a, b, i):
+    funct7 = "1001111"
+    funct3 = "000"
+    f = open("../computeTB.bsv", "a")
+    f.write("""\n\t\tif(counter == {j}) $display("[TB {j}] %b", fn_compute(PBoxIn{o}instr: 32'b{f7}{rs1addr}{rs2addr}{f3}{rdaddr}{opcode}, rs1: 64'd{rs1}, rs2: 64'd{rs2}, rd:  64'd{rd}{c}).data);"""\
+            .format(f7=funct7, f3=funct3, rs1=a, rs2=b, rd=0, rs1addr="00000", rs2addr="00000", rdaddr="00000", opcode="1110111", o='{', c='}', j=i))
+    f.close()
+
+    a8s = list((a >> j) & 0xFF for j in range(0,64,8))
+    b8s = list((b >> j) & 0xFF for j in range(0,64,8))
+    if(debug): print(a8s, b8s)
+    res = (mul8(a8s[7], b8s[6], True, True)[0]>>7 & 0xF) + sep +\
+          (mul8(a8s[6], b8s[7], True, True)[0]>>7 & 0xF) + sep +\
+          (mul8(a8s[5], b8s[4], True, True)[0]>>7 & 0xF) + sep +\
+          (mul8(a8s[4], b8s[5], True, True)[0]>>7 & 0xF) + sep +\
+          (mul8(a8s[3], b8s[2], True, True)[0]>>7 & 0xF) + sep +\
+          (mul8(a8s[2], b8s[3], True, True)[0]>>7 & 0xF) + sep +\
+          (mul8(a8s[1], b8s[0], True, True)[0]>>7 & 0xF) + sep +\
+          (mul8(a8s[0], b8s[1], True, True)[0]>>7 & 0xF)
     if(debug): print(res)
     return res
